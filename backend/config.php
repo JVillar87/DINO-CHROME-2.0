9b011d7d7d8e59af5ba9072642477e6538d73b51
JENNIFER <jennifer.villar@estudiant.fjaverianas.com>
2026-05-11 12:41:19 +0200

    v2 - Hosting

--- backend/config.php
+++ backend/config.php
@@ -8,9 +8,10 @@ function getDinoChrome(): mysqli
     $dbname = "dinocrome";
 
     //$connection = new mysqli($servername, $username, $password, $dbname);
-    $connection = new mysqli("sql101.infinityfree.com", "if0_41887203", "RPGw6HnfUhfpOSe", "if0_41887203_dinocrome"); 
+    $connection = new mysqli("sql101.infinityfree.com", "if0_41887203", "RPGw6HnfUhfpOSe", "if0_41887203_dinocrome"); //BBDD INFINITYFREE (Hosting)
     if ($connection->connect_error) {
         die("Connection failed: " . $connection->connect_error);
     }
     return $connection;
 }
+?>
